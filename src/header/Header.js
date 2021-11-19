780f39b6073bb92dec97206dc3d6a5e5239a4a94
Bogdan <bgra4ov@gmail.com>
2021-11-19 15:03:58 +0200

    Fight Ambition Team

--- src/header/Header.js
+++ src/header/Header.js
@@ -2,15 +2,53 @@ import React from 'react';
 import './Header-style/Header.scss';
 import logo from './Header-img/logo.png';
 function MenuItems(){
-    const links = ["главная","цены","расписание","о нас","грєпплинг","тайский бокс","мма","конакты","faq"];
-    const linksItems = links.map((item,index,array) => 
-    <li>{item}</li>
+    const links = [{
+        id: 1,
+        link: "главная"
+    },
+    {
+        id: 2,
+        link: "цены"
+    },
+    {
+        id: 3,
+        link: "расписание"
+    },
+    {
+        id: 4,
+        link: "о нас"
+    },
+    {
+        id: 5,
+        link: "грепплинг"
+    },
+    {
+        id: 6,
+        link: "тайский бокс"
+    },
+    {
+        id: 7,
+        link: "мма"
+    },
+    {
+        id: 8,
+        link: "конакты"
+    },
+    {
+        id: 9,
+        link: "faq"
+    }];
+    const linksItems = links.map((item) => 
+    <li key={item.id}>{item.link}</li>
     );
     function ButtonTitle(props){
         return <button type="button">{props.title}</button>;
     }
     return(
     <header>
+        <div className="MobMenuOpener">
+        <span>&#9776;</span>
+        </div>
         <div className="logo">
             <img src={logo}  alt="logo"/>
             </div>
